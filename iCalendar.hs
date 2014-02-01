23efe8b8dbfc7ad1b775f67a2ad6e399f2d7bc7c
FPtje <fpeijnenburg@gmail.com>
2014-02-01 19:59:38 +0100

    iCalendar main file fix

--- iCalendar.hs
+++ iCalendar.hs
@@ -1,30 +1,31 @@
 module Main where
-import CalendarParse
-import ParserExtend
 import System.IO
 import Data.Maybe
 
 import CalendarTypes
+import CalendarParse
 import DateTypes
 import EventTypes
+import Text.ParserCombinators.UU.Utils
+import Text.ParserCombinators.UU.BasicInstances
 
 
 -- Test:
 testString = "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B2@example.com\r\nDTSTAMP:19970610T172345Z\r\nDTSTART:19970714T170000Z\r\nDTEND:19970715T040000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B3@example.com\r\nDTSTAMP:19990610T172345Z\r\nDTSTART:19940714T170000Z\r\nDTEND:19970715T040000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B2@example.com\r\nDTSTAMP:19970610T172345Z\r\nDTSTART:19970714T170000Z\r\nDTEND:19970715T040000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B3@example.com\r\nDTSTAMP:19990610T172345Z\r\nDTSTART:19940714T170000Z\r\nDTEND:19970715T040000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n"
 
 
-testCalendar = fromJust $ run parseCalendar testString
-testDateTime = DateTime (Date (Year One Nine Nine Seven) (DoubleDigit Zero Seven) (DoubleDigit One Four)) (Time (DoubleDigit One Eight) (DoubleDigit Zero Four) (DoubleDigit Five Nine) UTC)
+testCalendar = execParser parseCalendar testString
+testDateTime  = DateTime (Date 1997 07 04) (Time 18 04 59 UTC)
 
-testDateTime1 = DateTime (Date (Year Two Zero Zero Two) (DoubleDigit Zero Seven) (DoubleDigit One Four)) (Time (DoubleDigit One Eight) (DoubleDigit One Five) (DoubleDigit Zero Zero) UTC)
+testDateTime1 = DateTime (Date 2002 07 04) (Time 18 06 00 UTC)
 
-testDateTime2 = DateTime (Date (Year Two Zero Zero Zero) (DoubleDigit Zero Seven) (DoubleDigit One Four)) (Time (DoubleDigit One Eight) (DoubleDigit Zero Four) (DoubleDigit Five Nine) UTC)
+testDateTime2 = DateTime (Date 2000 07 04) (Time 18 04 59 UTC)
 
-testDateTime3 = DateTime (Date (Year Two Zero Zero One) (DoubleDigit Zero Seven) (DoubleDigit One Four)) (Time (DoubleDigit One Eight) (DoubleDigit Zero Four) (DoubleDigit Five Nine) UTC)
+testDateTime3 = DateTime (Date 2001 07 04) (Time 18 04 59 UTC)
 
-testDateTime4 = DateTime (Date (Year Two Zero Zero One) (DoubleDigit Zero Seven) (DoubleDigit One Four)) (Time (DoubleDigit One Eight) (DoubleDigit Zero Four) (DoubleDigit Five Nine) UTC)
+testDateTime4 = DateTime (Date 2001 07 04) (Time 18 04 59 UTC)
 
-testDateTime5 = DateTime (Date (Year Two Zero Zero One) (DoubleDigit Zero Seven) (DoubleDigit One Four)) (Time (DoubleDigit One Nine) (DoubleDigit Zero Four) (DoubleDigit Five Nine) UTC)
+testDateTime5 = DateTime (Date 2001 07 04) (Time 19 04 59 UTC)
 
 testEvent1 = Event [DtStart testDateTime, DtEnd testDateTime1]
 
@@ -34,12 +35,14 @@ testEvent2 = Event [DtStart testDateTime2, DtEnd testDateTime3]
 {-
 	IO
 -}
-readCalendar :: FilePath -> IO (Maybe Calendar)
+readCalendar :: FilePath -> IO (Calendar, [Error LineColPos])
 readCalendar path = do
 	handle 		<- openFile path ReadMode
 	hSetNewlineMode handle noNewlineTranslation
 
 	contents 	<- hGetContents handle
-	return (run parseCalendar contents)
+	putStr ""
+	let res = execParser parseCalendar contents
+	return res
 
 main = putStr ""
