b0f1141713bfb331c3f53b4b08d66f10a77d5c67
FPtje <fpeijnenburg@gmail.com>
2014-02-01 19:44:08 +0100

    Fixed up Calendar types

--- CalendarTypes.hs
+++ CalendarTypes.hs
@@ -13,19 +13,19 @@ data Calprop    = Version
 -}
 
 -- Calendar
-type CalendarAlgebra cal calprop e ep dt date time year doubledigit digit utc = (
+type CalendarAlgebra cal calprop e ep dt date time utc = (
 		[calprop] -> [e] -> cal,
 		CalpropAlgebra calprop,
-		EventAlgebra e ep dt date time year doubledigit digit utc
+		EventAlgebra e ep dt date time utc
 	)
 
-foldCalendar :: CalendarAlgebra cal calprop e ep dt date time year doubledigit digit utc -> Calendar -> cal
+foldCalendar :: CalendarAlgebra cal calprop e ep dt date time utc -> Calendar -> cal
 foldCalendar (cal, prop, event) = fold
 	where
 		fold (Calendar props events) = cal (map (foldCalprop prop) props) (map (foldEvent event) events)
 
 -- id for Calendar
-idCalendar :: CalendarAlgebra Calendar Calprop Event EventProp DateTime Date Time Year DoubleDigit Digit TimeUTC
+idCalendar :: CalendarAlgebra Calendar Calprop Event EventProp DateTime Date Time TimeUTC
 idCalendar = (Calendar, idCalProp, idEvent)
 
 -- Calendar property
@@ -62,26 +62,3 @@ printCalendar = show
 -- Get the amount of events in a calendar
 eventCount :: Calendar -> Int
 eventCount (Calendar _ es) = length es
-
--- Get the events on a certain date and time
-dateEvents :: Calendar -> DateTime -> [Event]
-dateEvents (Calendar _ events) dateTime = filter ((flip isOnDate) dateTime) events
-
--- Get overlapping events. Returns a list of event pairs that overlap.
-overlappingEventsAlg :: CalendarAlgebra [(Event, Event)] Calprop Event EventProp DateTime Date Time Year DoubleDigit Digit TimeUTC
-overlappingEventsAlg = (\_ events -> [(x, y) | x <- events, y <- events, x `overlaps` y, x /= y],
-	idCalProp,
-	idEvent)
-
-overlappingEvents :: Calendar -> [(Event, Event)]
-overlappingEvents = foldCalendar overlappingEventsAlg
-
--- get events that have a certain summary
-findBySummary :: Calendar -> String -> [Event]
-findBySummary cal summ = foldCalendar (\_ -> filter $ (summ ==) . summary, idCalProp, idEvent) cal
-
--- Find the total time spent on all events with the given summary. The answer is in seconds.
-getSpentTimeOn :: Calendar -> String -> Integer
-getSpentTimeOn cal summ = foldl (+) 0 $ map getDuration events
-	where
-		events = findBySummary cal summ
