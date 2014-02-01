b2e6cbf2b02594531cbb803ee8390924e11c3faf
FPtje <fpeijnenburg@gmail.com>
2014-02-01 19:17:29 +0100

    Changed DateTime parser to use uu-parsinglib

--- DateParse.hs
+++ DateParse.hs
@@ -1,36 +1,40 @@
+{-# LANGUAGE  FlexibleInstances,
+              TypeSynonymInstances,
+              MultiParamTypeClasses,
+              Rank2Types, FlexibleContexts, NoMonomorphismRestriction,
+              CPP  #-}
+
 module DateParse (parseDateTime) where
-import ParseLib.Abstract
+import Text.ParserCombinators.UU
+import Text.ParserCombinators.UU.Utils
+import Text.ParserCombinators.UU.BasicInstances
 import DateTypes
 
 -- DateTime
-pDateTime :: Parser Char DateTime
+pDateTime :: Parser DateTime
 pDateTime = DateTime <$> pDate <* pDateSep <*> pTime
 parseDateTime = pDateTime -- Alias
 
 -- Date separator
-pDateSep :: Parser Char Char
-pDateSep = symbol 'T'
+pDateSep :: Parser Char
+pDateSep = pSym 'T'
 
 -- Date parsing
-pDate :: Parser Char Date
-pDate = Date <$> pYear <*> pDoubleDigit <*> pDoubleDigit
+pDate :: Parser Date
+pDate = Date <$> pQuadDigit <*> pDoubleDigit <*> pDoubleDigit
 
 -- Time parsing
-pTime :: Parser Char Time
+pTime :: Parser Time
 pTime = Time <$> pDoubleDigit <*> pDoubleDigit <*> pDoubleDigit <*> pUtcTime
 
--- Year parsing
-pYear :: Parser Char Year
-pYear = Year <$> pDigit <*> pDigit <*> pDigit <*> pDigit
+pQuadDigit :: Parser Int
+pQuadDigit = (\n1 n2 n3 n4 -> n1 * 1000 + n2 * 100 + n3 * 10 + n4) <$> pDigitAsNum <*> pDigitAsNum <*> pDigitAsNum <*> pDigitAsNum
 
 -- Parsing months, days, hours, minutes and seconds
-pDoubleDigit :: Parser Char DoubleDigit
-pDoubleDigit = DoubleDigit <$> pDigit <*> pDigit
+pDoubleDigit :: Parser Int
+pDoubleDigit = (\l r -> l * 10 + r) <$> pDigitAsNum <*> pDigitAsNum
 
 -- UTC Time parsing
-pUtcTime :: Parser Char TimeUTC
-pUtcTime = UTC <$ symbol 'Z' <<|> succeed TimeLocal
+pUtcTime :: Parser TimeUTC
+pUtcTime = UTC <$ pSym 'Z' `opt` TimeLocal
 
--- Parse a digit Char to a Digit datastructure instance
-pDigit :: Parser Char Digit
-pDigit = chrToDigit <$> digit
