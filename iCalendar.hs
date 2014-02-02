474de33f258e1a6af444002370166d680ad5fe4f
FPtje <fpeijnenburg@gmail.com>
2014-02-02 11:57:05 +0100

    Removed test data

--- iCalendar.hs
+++ iCalendar.hs
@@ -13,29 +13,6 @@ import PrintCalendar
 import Text.ParserCombinators.UU.Utils
 import Text.ParserCombinators.UU.BasicInstances
 
-
--- Test:
-testString = "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B2@example.com\r\nDTSTAMP:19970610T172345Z\r\nDTSTART:19970714T170000Z\r\nDTEND:19970715T040000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B3@example.com\r\nDTSTAMP:19990610T172345Z\r\nDTSTART:19940714T170000Z\r\nDTEND:19970715T040000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B2@example.com\r\nDTSTAMP:19970610T172345Z\r\nDTSTART:19970714T170000Z\r\nDTEND:19970715T040000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B3@example.com\r\nDTSTAMP:19990610T172345Z\r\nDTSTART:19940714T170000Z\r\nDTEND:19970715T040000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n"
-
-
-testCalendar = execParser parseCalendar testString
-testDateTime  = DateTime (Date 1997 07 04) (Time 18 04 59 UTC)
-
-testDateTime1 = DateTime (Date 2002 07 04) (Time 18 06 00 UTC)
-
-testDateTime2 = DateTime (Date 2000 07 04) (Time 18 04 59 UTC)
-
-testDateTime3 = DateTime (Date 2001 07 04) (Time 18 04 59 UTC)
-
-testDateTime4 = DateTime (Date 2001 07 04) (Time 18 04 59 UTC)
-
-testDateTime5 = DateTime (Date 2001 07 04) (Time 19 04 59 UTC)
-
-testEvent1 = Event [DtStart testDateTime, DtEnd testDateTime1]
-
-testEvent2 = Event [DtStart testDateTime2, DtEnd testDateTime3]
-
-
 {-
 	IO
 -}
